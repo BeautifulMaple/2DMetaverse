e992164775585a71e56e82e63ca740262339f845
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-02-18 19:25:46 +0900

    [feat] 플레이어 따라가기
    FollowCamera.cs
    - 초기 오프셋을 저장하여 실시간으로 카메라가 타겟을 따라가게 코드 구현

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform targetTrans;
+    float offsetX;
+    float offsetY;
+
+    private void Start()
     {
-        
+        if (targetTrans == null) return;  // 대상이 없으면 실행하지 않음
+        offsetX = transform.position.x - targetTrans.position.x; // 초기 오프셋 저장
+        offsetY = transform.position.y - targetTrans.position.y; // 초기 오프셋 저장
+
+    }
+    private void Update()
+    {
+        if (targetTrans == null) return;
+
+        Vector3 pos = transform.position;
+        pos.x = targetTrans.position.x + offsetX;
+        pos.y = targetTrans.position.y + offsetY;
+        transform.position = pos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
